5e3e6cc924b8d30b82e80c032be0c854b6e3b262
Thiago Torres <TheThiagoo0@gmail.com>
2016-02-18 12:24:49 -0200

    Element Class Almost over
    By 89%, I'd say.

--- ChainedListApplication/ChainedListApplication/Element.cs
+++ ChainedListApplication/ChainedListApplication/Element.cs
@@ -4,8 +4,72 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+/// <summary>
+/// 
+/// </summary>
+
 namespace ChainedListApplication {
     class Element {
+        
+        #region properties 
 
+        private Element next;
+
+        private string name;
+        private int age;
+        private char sex;
+
+        private Element previous;
+
+        #endregion
+
+        #region constructors
+        public Element(string name, int age, char sex)
+        {
+            Name = name;
+            Age = age;
+            Sex = sex;
+            Next = null;
+            Previous = null;
+        }
+
+        public Element(string name, int age, char sex, Element n = null)
+        {
+            Name = name;
+            Age = age;
+            Sex = sex;
+            Next = n;
+            Previous = null;
+        }
+
+        public Element(string name, int age, char sex, Element n = null, Element p = null) 
+        {
+            Name = name;
+            Age = age;
+            Sex = sex;
+            Next = n;
+            Previous = p;
+        }
+        #endregion
+        public string Name;
+        public int Age;
+        public char Sex;
+        public Element Next {
+            get {
+                return Next;
+            }
+            set {
+                Next = value;
+            }
+        }
+
+        public Element Previous {
+            get {
+                return Previous;
+            }
+            set {
+                Previous = value;
+            }
+        }
     }
 }
